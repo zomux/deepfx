f11f5c63e5a709a51d42a1cb138703fa2e0f7d2e
root <zomux@qq.com>
2018-01-30 21:57:07 +0900

    -

--- deepfxsite/views.py
+++ deepfxsite/views.py
@@ -1,12 +1,8 @@
-import os
 from django.shortcuts import render
-from django.conf import settings
 from django.http import HttpResponse
-
-from . import database
-from .models import PageView
-
 # Create your views here.
+import os
+
 
 def index(request):
     hostname = os.getenv('HOSTNAME', 'unknown')
--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from welcome.views import index, health
+from deepfxsite.views import index, health
 
 urlpatterns = [
     # Examples:
@@ -11,7 +11,7 @@ urlpatterns = [
 
     url(r'^$', index),
     url(r'^health$', health),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
